15d1a821421dfb74cd22c55db97405d6045a5b51
A <leandro.sierra@tarify.com>
2024-10-09 16:30:22 +0200

    Ahora si nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express()
 
 app.get("/",(req,res) => {
-    res.send('<h1>Pagina principal</h1>')
+    res.send('<h1>Pagina principal para David y Agus</h1>')
 })
 app.get("/about",(req,res) => {
     res.send('<h1>Estás en about</h1>')
