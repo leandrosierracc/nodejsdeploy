e7f2a02e89c28a176bc6185cb69897c09933405b
A <leandro.sierra@tarify.com>
2024-10-09 11:06:54 +0200

    segundo intento

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express()
+
+app.get("/",(req,res) => {
+    res.send('<h1>Pagina principal</h1>')
+})
+app.get("/about",(req,res) => {
+    res.send('<h1>Estás en about</h1>')
+})
+
+app.listen(3000)
+
+console.log("server funcionandoooo")
+
